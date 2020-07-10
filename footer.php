7b50dd396bf4ac89cc6773de6d5098ac0455b5bf
Khattech <shivamkhattri123@gmail.com>
2020-07-10 13:00:48 +0530

    Updated

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer"> 
+    <div class="container">
+       <center>
+         <p>Copyright &copy Lifestyle Store. All Rights Reserved by Shivam Khattri. | Contact Us: +91 8630939296</p>        
+       </center>
+    </div>
+</footer>
